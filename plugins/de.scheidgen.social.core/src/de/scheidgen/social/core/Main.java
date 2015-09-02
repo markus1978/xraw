b263038d0ff148c6cb3dd3604b41e876c355491e
Markus Scheidgen <markus.scheidgen@gmail.com>
2015-09-02 23:58:22 +0200

    Initial commit of a first example.

---
+++ plugins/de.scheidgen.social.core/src/de/scheidgen/social/core/Main.java
@@ -0,0 +1,75 @@
+package de.scheidgen.social.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.scribe.builder.api.Api;
+import org.scribe.builder.api.TumblrApi;
+import org.scribe.builder.api.TwitterApi;
+
+import de.scheidgen.social.core.socialstore.Profile;
+import de.scheidgen.social.core.socialstore.SocialStoreFactory;
+import de.scheidgen.social.core.socialstore.SocialStorePackage;
+
+public class Main {
+
+	private static final String DATA_STORE_PATH = "data/store.xmi";
+	
+	public static void main(String[] args) {
+		EPackage.Registry.INSTANCE.put(SocialStorePackage.eINSTANCE.getNsURI(), SocialStorePackage.eINSTANCE);		
+		EPackage.Registry.INSTANCE.put(EcorePackage.eINSTANCE.getNsURI(), EcorePackage.eINSTANCE);
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		
+		URI storeURI = URI.createFileURI(DATA_STORE_PATH);
+		ResourceSet rs = new ResourceSetImpl();
+		Resource resource = null;
+		if (new File(DATA_STORE_PATH).exists()) {
+			resource = rs.getResource(storeURI, true);
+		} else {
+			resource = rs.createResource(storeURI);
+		}
+		
+		Profile profile = null;
+		if (!resource.getContents().isEmpty()) {
+			profile = (Profile)resource.getContents().get(0);
+		}
+		if (profile == null) {
+			@SuppressWarnings("resource")
+			Scanner in = new Scanner(System.in);
+			System.out.println("No profile found. We will create one.");
+			System.out.println("Choose a login:");
+			System.out.print(">>");
+			String profileId = in.nextLine().trim();
+			
+			profile = SocialStoreFactory.eINSTANCE.createProfile();
+			profile.setId(profileId);
+			resource.getContents().add(profile);
+		}
+		
+		tryService(profile, TwitterApi.class, "uObLVPxuBJqfrEOEB3ms1g", "IAYcfFI5Xhq6g0McCdPVM5EEFOqq8PUkPH7KQu58w", null, "https://api.twitter.com/1.1/account/verify_credentials.json");
+		tryService(profile, TumblrApi.class, "9L2yRmNiq025MEfqJrbRpozRF2YQXwaOtW3e9exyL9eVrubc4b", "pogU7YRqOPyX5FkMaNwrLQea2MgGxnzqDIbcN5QDoLvHZ3lD8N", "http://www.tumblr.com/connect/login_success.html", "http://api.tumblr.com/v2/user/info");
+		
+		try {
+			resource.save(null);
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}
+	}
+	
+	private static void tryService(Profile profile, Class<? extends Api> apiClass, String key, String secret, String callbackURL, String testURL) {
+		SocialService twitter = SocialService.authenticate(profile, apiClass, key, secret, callbackURL);
+		String response = twitter.get(testURL);
+		System.out.println("Example response from " + apiClass.getSimpleName() + ":");
+		System.out.println(response);
+		System.out.println("");
+	}
+
+}
